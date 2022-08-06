cff43d34555cf2b87aa694d9f44000f9817eeaec
Gleb <example@gmail.com>
2022-08-06 21:49:19 +0300

    first try commiting

---
+++ js/12 lesson.js
@@ -0,0 +1,27 @@
+const arrObj = {
+    a: 1,
+    1: 2
+};
+
+
+arrObj.hello = 'world';
+
+console.log(arrObj.a);
+console.log(arrObj[1]);
+console.log(arrObj.hello);
+
+
+
+//Practice on Platform Udemy
+
+let storeName = 'Metro';
+let storeDescription = {
+    budget: 10000,
+    employees: ['Bill', 'John', 'Marci'],
+    products:{
+        cola: 100,
+        pepsi: 90
+    },
+    open: true
+};
+
---
+++ js/script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+//const obj = {
+    //name: 'John',
+    //age: 25,
+   // isMarried: false
+//};
+
+//console.log(obj ['name']);
+
+//let arr = ['apple.png', 26, 27, 'grusha.png'];
+
+//console.log(arr[1]);
+
+
+//alert('Hello');
+
+//const result = confirm('Are you here?');
+//console.log(result);
+
+//const answer = +prompt("Сколько вам лет?", "");
+//console.log(answer+5);
+//const answers = [];
+//answers[0] = prompt('Как ваше имя?', '');
+//answers[1] = prompt('Как ваша фамилия?', '');
+//answers[2] = prompt('Сколько вам лет?', '');
+
+//console.log(typeof(answers));
+//document.write(answers);
+
+
+//const user = 'Ivan';
+
+//alert(`Привет, ${user}`);
+
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+console.log(--incr);
+
+console.log(5%2);
+
+console.log(2*4 == 8);
+console.log(5*5 === '25');
+
+
+const isChecked = true,
+      isClose = true;
+
+console.log(isChecked && isClose);
+
+console.log(isClose || !isChecked);
+
+
+console.log(2 + 2 * 2 !== 6);
