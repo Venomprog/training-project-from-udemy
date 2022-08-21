94ea00d63aa84958c4ccf5d6720dd638c86ffe45
VenomProg <glebshaevvv@mail.ru>
2022-08-21 23:53:29 +0300

    cicles

--- js/script.js
+++ js/script.js
@@ -167,14 +167,25 @@ console.log(2 + 2 * 2 !== 6);
 // }
 
 
-let result = '';
-const lengthOfStars = 7;
+// let result = '';
+// const lengthOfStars = 7;
 
-for (let i = 1; i < lengthOfStars; i++){
-    for (let j = 0; j < i; j++){
-        result += "*";
-    }
-    result += "\n";
-}
+// for (let i = 1; i < lengthOfStars; i++){
+//     for (let j = 0; j < i; j++){
+//         result += "*";
+//     }
+//     result += "\n";
+// }
 
-console.log(result);
+// console.log(result);
+
+first: for (let i = 0; i < 3; i++){
+        console.log(`First level: ${i}`);
+        for (let j = 0; j < 3; j++){
+            console.log(`Second level: ${j}`);
+            for (let k = 0; k < 5; k++){
+                if (k === 2) continue first;
+                console.log(`Third level: ${k}`);
+            }
+         }
+     }
