02ab638965c5385e6b4b46d32e2fd997e7e5ae8d
VenomProg <glebshaevvv@mail.ru>
2022-08-21 23:54:59 +0300

    comments for code

--- js/script.js
+++ js/script.js
@@ -189,3 +189,15 @@ first: for (let i = 0; i < 3; i++){
             }
          }
      }
+
+
+     first: for (let i = 0; i < 3; i++){
+        console.log(`First level: ${i}`);
+        for (let j = 0; j < 3; j++){
+            console.log(`Second level: ${j}`);
+            for (let k = 0; k < 5; k++){
+                if (k === 2) break first;
+                console.log(`Third level: ${k}`);
+            }
+         }
+     }
