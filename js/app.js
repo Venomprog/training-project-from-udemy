8afbeb92647c0a0d7f6e794455f5b6c8868df5fa
VenomProg <glebshaevvv@mail.ru>
2022-09-04 23:19:28 +0300

    practice done

--- js/app.js
+++ js/app.js
@@ -1,92 +1,3 @@
-/* Закомментил т.к. выполнил практику по уроку
-
-
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-let lastFilm = prompt('Один из последних фильмов?', '');
-let rating = prompt('Оцените этот фильм', '');
-let a = prompt('Один из последних фильмов?', '');
-let b = prompt('Оцените этот фильм', '');
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies:{
-        
-    },
-    actors:{},
-    genres:[],
-    privat: false
-};
-personalMovieDB.movies[lastFilm] = rating;
-personalMovieDB.movies[a] = b;
-
-console.log(personalMovieDB);
-
-*/
-
-
-// function firstTask() {
-//     for (let i = 5; i < 11; i++){
-//         console.log(i);
-//     }
-// }
-// firstTask();
-
-
-// function secondTask() {
-//     for (let i = 20; i <= 20; i--){
-//         if (i === 13) break;
-//         console.log(i);
-//     }
-// }
-
-// secondTask();
-
-
-// function thirdTask() {
-//     for (let i = 2; i <=10; i++){
-//         if (i%2 === 0){
-//             console.log(i);
-//         }
-//     }
-// }
-
-// thirdTask();
-
-// for (let i = 2; i <= 16; i++) {
-//     if (i % 2 === 0) {
-//         continue;
-//     } else {
-//         console.log(i);
-//     }
-// }
-
-
-// let i = 2;
-// while (i < 16 ){
-//     i++;
-//     if (i % 2 === 0) {
-//         continue;
-//     } else {
-//         console.log(i);
-//     }
-    
-// }
-
-
-// function fifthTask() {
-//     const arrayOfNumbers = [];
-//     let j = 5;
-//     for (let i = 0; i <= 5; i++){
-//         arrayOfNumbers[i] = j;
-//         j++;
-//     }
-//     console.log(arrayOfNumbers);
-//     // Не трогаем
-//     return arrayOfNumbers;
-// }
-
-// fifthTask();
-
-
 
 let numberOfFilms;
 function start (){
@@ -107,8 +18,8 @@ const personalMovieDB = {
 
 function rememberMyFilms(){
     for (let i = 0; i < 2; i++){
-        let a = prompt('Один из последних фильмов?', '');
-        let b = prompt('Оцените этот фильм', '');
+        let a = prompt('Один из последних фильмов?', '').trim();
+        let b = prompt('Оцените этот фильм', '').trim();
         if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50){
             personalMovieDB.movies[a] = b;
             console.log('done');
