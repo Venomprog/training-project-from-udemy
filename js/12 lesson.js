3a1b5fd4ab16c20a03521d6e2104053b78d93f1c
VenomProg <glebshaevvv@mail.ru>
2022-09-05 22:43:19 +0300

    for in practice

--- js/12 lesson.js
+++ js/12 lesson.js
@@ -310,6 +310,20 @@ const options = {
     }
 };
 
-delete options.name;
+// delete options.name;
 
-console.log(options.name);
+// console.log(options.name);
+let counter = 0;
+for (let key in options){
+    if (typeof(options[key]) === 'object'){
+        for (let i in options[key]){
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+    }
+    
+}
+console.log(counter);
