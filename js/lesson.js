c7eddf70fa42defe93410d804d686e1bde780912
Shaev Gleb <75539098+Venomprog@users.noreply.github.com>
2022-10-09 22:19:54 +0300

    practice with cicles of nodes

--- js/lesson.js
+++ js/lesson.js
@@ -565,4 +565,11 @@
 
 //lesson 46
 
-console.log(document.body.childNodes);
+// console.log(document.body.childNodes); //Выводит все узлы и ноды у Боди
+
+for (let node of document.body.childNodes){
+    if (node.nodeName == '#text'){
+        continue;
+    }
+    console.log(node);
+}
